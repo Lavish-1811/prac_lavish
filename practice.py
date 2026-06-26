a585a0ec0e43ac27844db64f907b21ba2bae0a36
raj <rajgupta1811013@gmail.com>
2026-06-26 16:40:06 +0530

    new commit by raj

--- practice.py
+++ practice.py
@@ -2,3 +2,8 @@ print("hello world_is ")
 print("hii this is raj ")
 #this is done by raj 
 # this is the new change by raj
+
+print("hello world_is ")
+print("hii this is raj ")
+#this is done by raj 
+# this is the new change by raj
